e926ab46d92a1a960d92dde66e6e3a39d36e7785
Ahmad <asebak@users.noreply.github.com>
2016-01-19 17:17:45 -0800

    bdroid bluetooth configuration

---
+++ hardware/bluetooth/ bdroid_buildcfg.h
@@ -0,0 +1,11 @@
+#ifndef _BDROID_BUILDCFG_H
+#define _BDROID_BUILDCFG_H
+
+#define BTM_DEF_LOCAL_NAME   "BMI_BT"
+
+/* BLE */
+#define BTA_GATT_INCLUDED  TRUE
+#define BLE_INCLUDED       TRUE
+#define SMP_INCLUDED       TRUE
+
+#endif
