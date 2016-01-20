cd35228815e561a79b16696ad87490389c63a2b2
Ahmad <asebak@users.noreply.github.com>
2016-01-19 17:30:48 -0800

    added usb dongle

--- hardware/bluetooth/bdroid_buildcfg.h
+++ hardware/bluetooth/bdroid_buildcfg.h
@@ -1,7 +1,11 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "BBB-BT"
+#define BTM_DEF_LOCAL_NAME    "BBB-BT"
+
+/*USB Dongle*/
+
+#define BLUETOOTH_HCI_USE_USB   TRUE
 
 /* BLE */
 #define BTA_GATT_INCLUDED  TRUE
